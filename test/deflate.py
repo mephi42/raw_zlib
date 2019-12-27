793b352d9db12504ceb16d0e88f880cb7002b36c
mephi42 <mephi42@gmail.com>
2019-12-27 12:36:56 +0100

    Run tests before release

--- test/deflate.py
+++ test/deflate.py
@@ -4,28 +4,37 @@ import sys
 
 import raw_zlib
 
-strm = raw_zlib.z_stream(
-    zalloc=raw_zlib.Z_NULL, free=raw_zlib.Z_NULL, opaque=raw_zlib.Z_NULL)
-rc = raw_zlib.deflateInit(strm, raw_zlib.Z_DEFAULT_COMPRESSION)
-if rc != raw_zlib.Z_OK:
-    raise Exception('deflateInit() failed with error {}'.format(rc))
-stream_end = False
-obuf = ctypes.create_string_buffer(8192)
-while not stream_end:
-    ibuf = sys.stdin.buffer.read(16384)
-    strm.next_in = ibuf
-    strm.avail_in = len(ibuf)
-    flush = raw_zlib.Z_FINISH if strm.avail_in == 0 else raw_zlib.Z_NO_FLUSH
+
+def main():
+    strm = raw_zlib.z_stream(
+        zalloc=raw_zlib.Z_NULL, free=raw_zlib.Z_NULL, opaque=raw_zlib.Z_NULL)
+    rc = raw_zlib.deflateInit(strm, raw_zlib.Z_DEFAULT_COMPRESSION)
+    if rc != raw_zlib.Z_OK:
+        raise Exception('deflateInit() failed with error {}'.format(rc))
+    stream_end = False
+    obuf = ctypes.create_string_buffer(8192)
     while not stream_end:
-        if flush != raw_zlib.Z_FINISH and strm.avail_in == 0:
-            break
-        strm.next_out = ctypes.addressof(obuf)
-        strm.avail_out = ctypes.sizeof(obuf)
-        rc = raw_zlib.deflate(strm, flush)
-        stream_end = rc == raw_zlib.Z_STREAM_END and flush == raw_zlib.Z_FINISH
-        if rc != raw_zlib.Z_OK and not stream_end:
-            raise Exception('deflate() failed with error {}'.format(rc))
-        sys.stdout.buffer.write(obuf[:len(obuf) - strm.avail_out])
-rc = raw_zlib.deflateEnd(strm)
-if rc != raw_zlib.Z_OK:
-    raise Exception('deflateEnd() failed with error {}'.format(rc))
+        ibuf = sys.stdin.buffer.read(16384)
+        strm.next_in = ibuf
+        strm.avail_in = len(ibuf)
+        flush = (raw_zlib.Z_FINISH
+                 if strm.avail_in == 0
+                 else raw_zlib.Z_NO_FLUSH)
+        while not stream_end:
+            if flush != raw_zlib.Z_FINISH and strm.avail_in == 0:
+                break
+            strm.next_out = ctypes.addressof(obuf)
+            strm.avail_out = ctypes.sizeof(obuf)
+            rc = raw_zlib.deflate(strm, flush)
+            stream_end = (rc == raw_zlib.Z_STREAM_END and
+                          flush == raw_zlib.Z_FINISH)
+            if rc != raw_zlib.Z_OK and not stream_end:
+                raise Exception('deflate() failed with error {}'.format(rc))
+            sys.stdout.buffer.write(obuf[:len(obuf) - strm.avail_out])
+    rc = raw_zlib.deflateEnd(strm)
+    if rc != raw_zlib.Z_OK:
+        raise Exception('deflateEnd() failed with error {}'.format(rc))
+
+
+if __name__ == '__main__':
+    main()
--- test/inflate.py
+++ test/inflate.py
@@ -4,29 +4,35 @@ import sys
 
 import raw_zlib
 
-strm = raw_zlib.z_stream(
-    next_in=raw_zlib.Z_NULL, avail_in=0,
-    zalloc=raw_zlib.Z_NULL, free=raw_zlib.Z_NULL, opaque=raw_zlib.Z_NULL)
-rc = raw_zlib.inflateInit(strm)
-if rc != raw_zlib.Z_OK:
-    raise Exception('inflateInit() failed with error {}'.format(rc))
-stream_end = False
-obuf = ctypes.create_string_buffer(16384)
-while not stream_end:
-    ibuf = sys.stdin.buffer.read(8192)
-    strm.next_in = ibuf
-    strm.avail_in = len(ibuf)
+
+def main():
+    strm = raw_zlib.z_stream(
+        next_in=raw_zlib.Z_NULL, avail_in=0,
+        zalloc=raw_zlib.Z_NULL, free=raw_zlib.Z_NULL, opaque=raw_zlib.Z_NULL)
+    rc = raw_zlib.inflateInit(strm)
+    if rc != raw_zlib.Z_OK:
+        raise Exception('inflateInit() failed with error {}'.format(rc))
+    stream_end = False
+    obuf = ctypes.create_string_buffer(16384)
     while not stream_end:
-        strm.next_out = ctypes.addressof(obuf)
-        strm.avail_out = ctypes.sizeof(obuf)
-        rc = raw_zlib.inflate(strm, raw_zlib.Z_NO_FLUSH)
-        if rc == raw_zlib.Z_STREAM_END:
-            stream_end = True
-        elif rc == raw_zlib.Z_BUF_ERROR:
-            break
-        elif rc != raw_zlib.Z_OK:
-            raise Exception('inflate() failed with error {}'.format(rc))
-        sys.stdout.buffer.write(obuf[:len(obuf) - strm.avail_out])
-rc = raw_zlib.inflateEnd(strm)
-if rc != raw_zlib.Z_OK:
-    raise Exception('inflateEnd() failed with error {}'.format(rc))
+        ibuf = sys.stdin.buffer.read(8192)
+        strm.next_in = ibuf
+        strm.avail_in = len(ibuf)
+        while not stream_end:
+            strm.next_out = ctypes.addressof(obuf)
+            strm.avail_out = ctypes.sizeof(obuf)
+            rc = raw_zlib.inflate(strm, raw_zlib.Z_NO_FLUSH)
+            if rc == raw_zlib.Z_STREAM_END:
+                stream_end = True
+            elif rc == raw_zlib.Z_BUF_ERROR:
+                break
+            elif rc != raw_zlib.Z_OK:
+                raise Exception('inflate() failed with error {}'.format(rc))
+            sys.stdout.buffer.write(obuf[:len(obuf) - strm.avail_out])
+    rc = raw_zlib.inflateEnd(strm)
+    if rc != raw_zlib.Z_OK:
+        raise Exception('inflateEnd() failed with error {}'.format(rc))
+
+
+if __name__ == '__main__':
+    main()
