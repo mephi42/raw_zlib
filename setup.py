726886f7a8686d468641eb6dbc09fa005dd7c02d
mephi42 <mephi42@gmail.com>
2020-08-14 15:16:07 +0200

    Fix 'classifiers' should be a list, got type 'tuple'

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open('README.rst') as fp:
 
 setuptools.setup(
     name='raw_zlib',
-    version='0.1.10',
+    version='0.1.11',
     author='mephi42',
     author_email='mephi42@gmail.com',
     description='Thin Python 3 wrapper around zlib',
     long_description=long_description,
     url='https://github.com/mephi42/raw_zlib',
     packages=setuptools.find_packages(),
-    classifiers=(
+    classifiers=[
         'Programming Language :: Python',
         'License :: OSI Approved :: BSD License',
         'Operating System :: POSIX',
-    ),
+    ],
 )
